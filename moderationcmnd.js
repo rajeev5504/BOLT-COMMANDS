0982cdd9103fb9ccba19e98476e846ab09f7fc51
rajeev5504 <67921625+rajeev5504@users.noreply.github.com>
2020-10-05 12:59:18 +0530

    Create moderationcmnd.js

---
+++ moderationcmnd.js
@@ -0,0 +1,17 @@
+kick       {usage : .kick @user or [userid] reason}
+ban        {usage : .ban @user or [userid] reason}
+mute       {usage : .mute @user or [userid] reason}
+unmute     {usage : .unmute @user reason}
+warns      {usage : .warns @user [checking your warns]}
+warn       {usage : .warn @user or [userid] reason}
+slowmode   {usage : .slowmode time [exmp 10]}
+unban      {usage : .unban @userid reason}
+nick       {usage : .nick @user nickname}
+addrole    {usage : .addrole @user reason}
+removerole {usage : .removerole @user reason}
+purge      {usage : .purge [number of messages]}
+clearwarn  {usage : .clearwarn @user reason}
+lockdown   {usage : .lockdown timing [exmp : 1sec/1min/1hour}
+say        {usage : .say message}
+tempmute   {usage : .tempmute @user reason}
+
