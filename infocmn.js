ac258010b3bd54673fda2083093d9202f7a716db
rajeev5504 <67921625+rajeev5504@users.noreply.github.com>
2020-10-05 13:19:35 +0530

    Create infocmn.js

---
+++ infocmn.js
@@ -0,0 +1,16 @@
+calculator   {usage : .calculator 2+2 }    
+ping         {usage : .ping [latency check]}
+uptime       {usage : .uptime bot uptiming}
+serverrole   {usage : .serverrole [how many roles in your server]}
+anime        {usage : .anime type your anime character}
+banlist      {usage : .banlist check how many peoples are banned on your server}
+botinfo      {usage : .botinfo infomation of bot}
+channelid    {usage : .channelid }
+channelinfo  {usage : .kick @user or [userid] reason}
+emoji        {usage : .emoji [emojiname]}
+userinfo     {usage : .userinfo @user [info of user]}
+roleinfo     {usage : .roleinfo }
+serverinfo   {usage : .serverinfo [server info]}
+spotify      {usage : .spotify @user}
+stats        {usage : .stats}
+youtubestats {usage : .youtubestats your youtube channel stats}
